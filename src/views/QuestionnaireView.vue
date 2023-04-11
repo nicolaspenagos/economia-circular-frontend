ce44972ca80c249e32914e06deba5866a8aadfb8
nicolaspenagos <nicolas.penagosm98@gmail.com>
2023-04-11 00:13:06 -0500

    feat(Onboarding):Onboarding component is under development

--- src/views/QuestionnaireView.vue
+++ src/views/QuestionnaireView.vue
@@ -1,18 +1,44 @@
 <script setup>
 import { mapStores } from "pinia";
 import { useAuthStore } from "../stores/auth";
+import Onboarding from "../components/ui_utils/Onboarding.vue";
 </script>
 <template>
+  <Onboarding v-if="showModal" :data="dataArray"/>
   <div></div>
 </template>
 <script>
 export default {
+emits: ["toggleHeader"],
+  data() {
+    return {
+        showModal:false,
+        dataArray:[{name:"Name", lastname:"Last"}, {name:"Name", lastname:"Last"}]
+    }
+  },
   computed: {
     ...mapStores(useAuthStore),
   },
+  methods: {
+    closeModal(){
+        this.showModal = false;
+    },
+    openModal() {
+        this.showModal = true;
+    },
+    handleOnboarding() {
+      const userId = this.authStore.user.id;
+      const currentUserOnboardingPath =
+        "questionnaireOnboardingCompleted" + userId;
+      if (!localStorage.getItem(currentUserOnboardingPath)) {
+        localStorage.setItem(currentUserOnboardingPath, "true");
+        this.openModal();
+      }
+    },
+  },
   mounted() {
-    console.log(this.authStore.user);
     this.$emit("toggleHeader", true);
+    this.handleOnboarding();
   },
 };
 </script>
